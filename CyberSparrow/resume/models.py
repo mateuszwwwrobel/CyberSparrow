c739c814cc49a81b427de166bb60abfd93733efc
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-03-22 17:25:46 +0000

    Add Education and Experience models

--- CyberSparrow/resume/models.py
+++ CyberSparrow/resume/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Education(models.Model):
+    title = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    start_date = models.DateField()
+    finish_date = models.DateField()
+    final_project = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.title} - {self.school}'
+
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    start_date = models.DateField()
+    finish_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.title} - {self.company}'

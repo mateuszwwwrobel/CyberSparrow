ef8506fb3edd5d8349a5974593c96415dc5fd08e
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-03-22 17:26:43 +0000

    Add resume views

--- CyberSparrow/resume/views.py
+++ CyberSparrow/resume/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
 from rest_framework import permissions
-from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
+from .models import Education, Experience
+from .serializers import ExperienceSerializer, EducationSerializer
+
 
 class ExperienceListView(ListAPIView):
-    pass
+    queryset = Experience.objects.order_by('-start_date')
+    serializer_class = ExperienceSerializer
+    permission_classes = (permissions.AllowAny,)
+
 
 class EducationListView(ListAPIView):
-    pass
+    queryset = Education.objects.order_by('-start_date')
+    serializer_class = EducationSerializer
+    permission_classes = (permissions.AllowAny,)

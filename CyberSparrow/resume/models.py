2369522ed60ea38765c1b7958a28f3c34a8d69e4
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-03-24 07:47:22 +0000

    Update fields on models

--- CyberSparrow/resume/models.py
+++ CyberSparrow/resume/models.py
@@ -5,8 +5,8 @@ class Education(models.Model):
     title = models.CharField(max_length=100)
     school = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-    start_date = models.DateField()
-    finish_date = models.DateField()
+    start_date = models.CharField(max_length=8)
+    finish_date = models.CharField(max_length=8)
     final_project = models.CharField(max_length=300)
     description = models.TextField()
 
@@ -18,8 +18,8 @@ class Experience(models.Model):
     title = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-    start_date = models.DateField()
-    finish_date = models.DateField()
+    start_date = models.CharField(max_length=8)
+    finish_date = models.CharField(max_length=8)
     description = models.TextField()
 
     def __str__(self):

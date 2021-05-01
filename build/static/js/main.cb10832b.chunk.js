fb414c4b069a52bb75fa295cd1c06e8ce84cb4f8
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-05-01 19:49:06 +0100

    troubleshooting

--- build/static/js/main.cb10832b.chunk.js
+++ build/static/js/main.cb10832b.chunk.js
@@ -17,7 +17,7 @@
                             to: "/",
                             children: Object(b.jsx)("img", {
                                 className: "img-fluid",
-                                src: "/staticfiles/img/logo_sparrow_small.png",
+                                src: "/static/img/logo_sparrow_small.png",
                                 alt: "logo"
                             })
                         }), Object(b.jsx)("button", {

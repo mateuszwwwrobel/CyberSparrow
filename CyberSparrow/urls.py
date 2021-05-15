d7a8da7b7a1d64967f2a38014d48bf85faabf531
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-05-15 09:26:19 +0100

    Bug fix

--- CyberSparrow/urls.py
+++ CyberSparrow/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,9 +25,7 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('api/blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-]
 
-# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-#               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
